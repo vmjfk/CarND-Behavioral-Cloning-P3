f9fb0e826e9dbc1531ea385a3a857d1fd5690229
vmjfk <vmjfk@kennedylaws.org>
2017-09-27 19:06:15 -0700

    must use GPU! taking too long

--- model.py
+++ model.py
@@ -31,15 +31,26 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda, Convolution2D, MaxPooling2D
 
 print("Building the model.")
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x /255.0 -0.5, input_shape=(160,320,3)))
+
+model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Dense(84))
 model.add(Dense(1))
 
-model.compile(loss='mse', optimizer='adam')
-model.fit(X_train,y_train,validation_split=0.2,shuffle=True)
 
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train,y_train,validation_split=0.2,shuffle=True,epochs=2)
+
+
+print("Saving model.h5")
 model.save('model.h5')
 
